97423303b934a6dc233bd435293c723fbc6591d2
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-28 18:09:20 +0900

    Title: 합과 평균, Time: 111ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250428/합과 평균/sum-and-avg.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.format("%d %.1f", a + b, (a + b)/2.0));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
