b921db2658f043a22c5587f6b13e86213233d8de
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-28 17:10:33 +0900

    Title: 변수 값 복사하기 3, Time: 83ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250428/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 1;
+        int b = 5;
+        int c = 3;
+
+        a = c;
+        a += c;
+        b -= c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
