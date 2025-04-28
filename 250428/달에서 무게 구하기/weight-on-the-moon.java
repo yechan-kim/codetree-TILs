386c3e3ca669571d969c01ee07576cf5c6c2c545
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-28 17:25:37 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250428/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        double weightRate = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", weight, weightRate, weight * weightRate);
+    }
+}
