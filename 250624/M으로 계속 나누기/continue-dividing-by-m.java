a4043d077f554389435d52613cec74bd91979a86
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-06-24 15:03:04 +0900

    Title: M으로 계속 나누기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250624/M으로 계속 나누기/continue-dividing-by-m.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        do {
+            System.out.println(N);
+
+            N/=M;
+        } while(N > 0);
+    }
+}
