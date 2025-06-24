e2d5c850bd9b6be847c897da757b819509a04465
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-06-24 14:28:17 +0900

    Title: 시험 통과 여부 확인하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250624/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 80) {
+            bw.write("pass");
+        } else {
+            int score = 80 - N;
+            bw.write(score + " more score");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
