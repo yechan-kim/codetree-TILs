3fa38223a2180d71687a4fec4549d5086046e0cb
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-06-25 15:52:19 +0900

    Title: 1부터의 합, Time: 98ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250625/1부터의 합/sum-from-the-one.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val N = readLine()!!.toInt()
+    var sum = 0
+
+    for (i in 1..N) {
+        sum += i
+        
+        if(sum > N){
+            print(i)
+
+            return
+        }
+    }
+}
