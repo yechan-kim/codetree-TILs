ba8a37db9027bc3d9a5a35ce4cfaaadcd23bd52f
Yechan Kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-30 03:16:38 +0900

    Title: 거품 정렬 구현, Time: 161ms, Memory: 17MB, Status: Passed - Codetree

--- 250730/거품 정렬 구현/implement-bubble-sort.kt
+++ 250730/거품 정렬 구현/implement-bubble-sort.kt
@@ -1,6 +1,6 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val arr = readln().split(" ").sorted()
+    val arr = readln().trim().split(" ").map { it.toInt() }.sorted()
     // Please write your code here.
 
     for(i in 0 until n){
